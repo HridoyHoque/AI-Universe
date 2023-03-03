64ebd764859798fb5a72d0d63574ce253a41e79b
HridoyHoque <hhridoy155@gmail.com>
2023-03-03 13:06:40 -0700

    fetch response coverted to json

---
+++ AI.js
@@ -0,0 +1,9 @@
+const loadAItools = () => {
+   url = `https://openapi.programming-hero.com/api/ai/tools`
+   fetch(url)
+   .then(res => res.json())
+   .then(data => console.log(data.data.tools))
+}
+
+
+loadAItools()
